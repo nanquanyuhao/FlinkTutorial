ebe148ed0167bac6fa7611852e07f17b156765a4
nanquanyuhao <nanquanyuhao@foxmail.com>
2021-01-28 17:38:08 +0800

    添加kafka消费示例

--- script/kafka.sh
+++ script/kafka.sh
@@ -1,5 +1,5 @@
 # 创建对应的 topic 为 sensor
 ./kafka-topics.sh --create --bootstrap-server 192.168.235.109:9092,192.168.235.110:9092,192.168.235.111:9092 --replication-factor 3 --partitions 3 --topic sensor
 
-# 查看已有的 topic
+# 对对应的 topic 生产消息
 ./kafka-console-producer.sh --broker-list 192.168.235.109:9092,192.168.235.110:9092,192.168.235.111:9092 --topic sensor
